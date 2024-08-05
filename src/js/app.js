9c5f6b2c3c3128a4cd9d58ae93a58048b4ba94d3
FerzGT <viktorlfes@rambler.ru>
2024-08-05 18:31:30 +0300

    исключение App

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import Game, { GameSavingData as loadGame, readGameSaving as saveGame } from './game.js';
+/*import Game, { GameSavingData as loadGame, readGameSaving as saveGame } from './game.js';
 
 const game = new Game();
-game.start();
+game.start();*/
